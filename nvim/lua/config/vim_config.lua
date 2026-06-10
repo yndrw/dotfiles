e6cc196b6eae816735a74bf0c64b94e1ddb67ca8
ydrew <lyao.andrew@gmail.com>
2026-06-10 11:43:03 -0400

    disable mouse

--- nvim/lua/config/vim_config.lua
+++ nvim/lua/config/vim_config.lua
@@ -14,7 +14,7 @@ vim.opt.scrolloff = 10
 vim.opt.incsearch = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
